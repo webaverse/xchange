856d0ba668d4a6b10cf27453c8cb9d520b1fbe3e
Savinkin <explosive0105@gmail.com>
2022-02-23 18:56:39 +0000

    SideBar - Play button added

--- layouts/components/nav-bar/index.js
+++ layouts/components/nav-bar/index.js
@@ -4,7 +4,7 @@ import Avatar from './Avatar'
 function NavBar() {
     return (
         <div className='w-min'>
-            <div className='w-[19vw] flex flex-col bg-stone-900 pt-6 p-[2.5vw] pt-6 shadow-[0px_0px_3px_3px]  overflow-x-hidden h-screen overflow-y-scroll'>
+            <div className='w-[19vw] flex flex-col bg-stone-900 pt-6 p-[2.5vw] pt-6 shadow-[0px_0px_3px_3px]  overflow-x-hidden h-screen hover:overflow-y-scroll overflow-y-hidden'>
                 <Avatar />
                 <Navigation />
             </div>
